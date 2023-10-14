69079d031e8401a1287e6e8ba37e3ad32aea2608
Beckett <133655553+severian42@users.noreply.github.com>
2023-10-14 23:10:13 +0000

    update css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import "./globals.css";
 import { Public_Sans } from "next/font/google";
 
@@ -5,38 +6,23 @@ import { Navbar } from "@/components/Navbar";
 
 const publicSans = Public_Sans({ subsets: ["latin"] });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <head>
+    <>
+      <Head>
         <title>ANIMA: Chat with your PDF</title>
         <link rel="shortcut icon" href="/images/favicon.ico" />
-        <meta
-          name="description"
-          content="Upload a PDF, then ask questions about it - without a single remote request!"
-        />
-        <meta
-          property="og:title"
-          content="Fully Local Chat Over Documents"
-        />
-        <meta
-          property="og:description"
-          content="Upload a PDF, then ask questions about it - without a single remote request!"
-        />
+        <meta name="description" content="Upload a PDF, then ask questions about it - without a single remote request!" />
+        <meta property="og:title" content="Fully Local Chat Over Documents" />
+        <meta property="og:description" content="Upload a PDF, then ask questions about it - without a single remote request!" />
         <meta property="og:image" content="/images/og-image.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-       
-        
-    
-       
-      <body className={publicSans.className}>
-        <div className="flex flex-col p-4 md:p-12 h-[100vh]">{children}</div>
-      </body>
-    </html>
+        <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@400;700&display=swap" rel="stylesheet" />
+      </Head>
+      <div className="flex flex-col p-4 md:p-12 h-[100vh]">
+        {children}
+      </div>
+    </>
   );
 }
