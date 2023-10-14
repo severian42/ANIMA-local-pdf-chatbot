e76b2832bc9c998c49c118dce348ec5de752d2f4
Beckett <133655553+severian42@users.noreply.github.com>
2023-10-14 23:19:07 +0000

    change

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,23 +6,45 @@ import { Navbar } from "@/components/Navbar";
 
 const publicSans = Public_Sans({ subsets: ["latin"] });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <>
-      <Head>
-        <title>ANIMA: Chat with your PDF</title>
+    <html lang="en">
+      <head>
+        <title>ANIMA - Chat With Your Documentss</title>
         <link rel="shortcut icon" href="/images/favicon.ico" />
-        <meta name="description" content="Upload a PDF, then ask questions about it - without a single remote request!" />
-        <meta property="og:title" content="Fully Local Chat Over Documents" />
-        <meta property="og:description" content="Upload a PDF, then ask questions about it - without a single remote request!" />
+        <meta
+          name="description"
+          content="Upload a PDF, then ask questions about it - without a single remote request!"
+        />
+        <meta
+          property="og:title"
+          content="Fully Client-Side Chat Over Documents"
+        />
+        <meta
+          property="og:description"
+          content="Upload a PDF, then ask questions about it - without a single remote request!"
+        />
         <meta property="og:image" content="/images/og-image.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-        <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@400;700&display=swap" rel="stylesheet" />
-      </Head>
-      <div className="flex flex-col p-4 md:p-12 h-[100vh]">
-        {children}
-      </div>
-    </>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Fully Client-Side Chat Over Documents"
+        />
+        <meta
+          name="twitter:description"
+          content="Upload a PDF, then ask questions about it - without a single remote request!"
+        />
+        <meta name="twitter:image" content="/images/og-image.png" />
+      </head>
+      <body className={publicSans.className}>
+        <div className="flex flex-col p-4 md:p-12 h-[100vh]">{children}</div>
+      </body>
+    </html>
   );
 }
