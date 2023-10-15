f8510c60588b8d9890723fe7ce5cee9e9f84c100
Beckett <133655553+severian42@users.noreply.github.com>
2023-10-15 11:54:42 +0000

    pdf

--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -3,7 +3,8 @@
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { useRef, useState, useEffect, FormEvent } from "react";
+import { useRef, useState, useEffect } from "react";
+import type { FormEvent } from "react";
 
 import { ChatMessageBubble } from "@/components/ChatMessageBubble";
 import { ChatWindowMessage } from '@/schema/ChatWindowMessage';
@@ -19,7 +20,6 @@ export function ChatWindow(props: {
   const [isLoading, setIsLoading] = useState(true);
   const [selectedPDF, setSelectedPDF] = useState<File | null>(null);
   const [readyToChat, setReadyToChat] = useState(false);
-  const [preloadedPDF, setPreloadedPDF] = useState<File | null>(null);
 
   const worker = useRef<Worker | null>(null);
 
@@ -60,13 +60,6 @@ export function ChatWindow(props: {
     });
 
   }
-  
-  async function preloadPDF() {
-    if (selectedPDF !== null) {
-      worker.current?.postMessage({ pdf: selectedPDF });
-    }
-  }
-  
 
   async function sendMessage(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -115,17 +108,14 @@ export function ChatWindow(props: {
 
   // We use the `useEffect` hook to set up the worker as soon as the `App` component is mounted.
   useEffect(() => {
-    // Fetch your default PDF here, either from local storage or some default path
-    const fetchDefaultPDF = async () => {
-      const response = await fetch("biomimetics-07-00103.pdf");
-      const blob = await response.blob();
-      const file = new File([blob], "default.pdf", { type: "application/pdf" });
-      setSelectedPDF(file);
-    };
-    fetchDefaultPDF();
+    if (!worker.current) {
+      // Create the worker if it does not yet exist.
+      worker.current = new Worker(new URL('../app/worker.ts', import.meta.url), {
+        type: 'module',
+      });
+      setIsLoading(false);
+    }
   }, []);
-  
-  
 
   async function embedPDF (e: FormEvent<HTMLFormElement>) {
     console.log(e);
@@ -165,8 +155,7 @@ export function ChatWindow(props: {
     };
     worker.current?.addEventListener("message", onMessageReceived);
   }
-  
-  
+
   const choosePDFComponent = (
     <>
       <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden flex flex-col">
@@ -203,17 +192,22 @@ export function ChatWindow(props: {
           <li className="text-l">
             👇
             <span className="ml-2">
-              Try embedding a PDF below, then asking questions! You can even turn off your WiFi.
+              Try embedding a PDF below, then asking questions! You can even turn off your WiFi. <a href="biomimetics-07-00103.pdf" download className="text-blue-500 hover:underline ml-2">
+                Download a sample PDF
+              </a>
             </span>
           </li>
         </ul>
       </div>
       <form onSubmit={embedPDF} className="mt-4 flex justify-between items-center w-full">
-      <input id="file_input" type="file" accept="pdf" className="text-white" onChange={(e) => e.target.files ? setSelectedPDF(e.target.files[0]) : null}></input>
+        <input id="file_input" type="file" accept="pdf" className="text-white" onChange={(e) => e.target.files ? setSelectedPDF(e.target.files[0]) : null}></input>
         <button type="submit" className="shrink-0 px-8 py-4 bg-sky-600 rounded w-28">
-          
-            {/* Your loading SVG here */}
-            <div role="status" className={`${isLoading ? "" : "hidden"} flex justify-center`}>
+          <div role="status" className={`${isLoading ? "" : "hidden"} flex justify-center`}>
+            <svg aria-hidden="true" className="w-6 h-6 text-white animate-spin dark:text-white fill-sky-800" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+            </svg>
+            <span className="sr-only">Loading...</span>
           </div>
           <span className={isLoading ? "hidden" : ""}>Embed</span>
         </button>
@@ -244,8 +238,12 @@ export function ChatWindow(props: {
             onChange={(e) => setInput(e.target.value)}
           />
           <button type="submit" className="shrink-0 px-8 py-4 bg-sky-600 rounded w-28">
-              {/* Your loading SVG here */}
-              <div role="status" className={`${isLoading ? "" : "hidden"} flex justify-center`}>
+            <div role="status" className={`${isLoading ? "" : "hidden"} flex justify-center`}>
+              <svg aria-hidden="true" className="w-6 h-6 text-white animate-spin dark:text-white fill-sky-800" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+              </svg>
+              <span className="sr-only">Loading...</span>
             </div>
             <span className={isLoading ? "hidden" : ""}>Send</span>
           </button>
@@ -255,12 +253,12 @@ export function ChatWindow(props: {
   );
 
   return (
-    <div className={`flex flex-col items-center p-4 md:p-8 rounded grow overflow-hidden ${readyToChat ? "border" : ""}`}>
+    <div className={`flex flex-col items-center p-4 md:p-8 rounded grow overflow-hidden ${(readyToChat ? "border" : "")}`}>
       <h2 className={`${readyToChat ? "" : "hidden"} text-2xl`}> {titleText}</h2>
-      {readyToChat ? chatInterfaceComponent : choosePDFComponent}
+      {readyToChat
+        ? chatInterfaceComponent
+        : choosePDFComponent}
       <ToastContainer/>
     </div>
   );
 }
-
-
